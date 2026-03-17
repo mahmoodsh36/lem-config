f6648e3bccda676a01e9824732797fe7bd15d544
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-03-17 22:05:05 +0200

    load extra/

--- init.lisp
+++ init.lisp
@@ -1,2 +1,8 @@
 (let ((asdf:*central-registry* (cons #P"~/.lem/" asdf:*central-registry*)))
   (asdf:load-system :lem-my-init))
+
+;; load extensions from extra/
+(let ((asdf:*central-registry* (cons #P"~/.lem/extra/" asdf:*central-registry*)))
+  (handler-case (asdf:load-system :lem-extra)
+    (error (c)
+      (format t "lem-extra failed to load: ~A~%" c))))
