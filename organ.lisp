6692d04d6fdf1e2cfb22ad9e19bf6966a630ba48
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-08-10 12:44:20 +0300

    small cleanup

--- organ.lisp
+++ organ.lisp
@@ -12,15 +12,6 @@
 ;; only show first instance of a repeated task
 (setf organ:*agenda-first-repeat-only* t)
 
-;; (setf organ:*organ-files*
-;;       `((:path (,(cltpt/file-utils:as-dir-path
-;;                   (cltpt/file-utils:join-paths
-;;                    (or (sb-ext:posix-getenv "WORK_DIR")
-;;                        (error "couldnt get BRAIN_DIR env var"))
-;;                    "cltpt/tests/")))
-;;          :glob "*.org"
-;;          :format "org-mode")))
-
 (led-key
  "E"
  (cmd
@@ -28,28 +19,6 @@
    (format nil
            "~A/cltpt/tests/test.org"
            (my-getenv "WORK_DIR")))))
-
-;; (defmethod asdf:perform :after ((op asdf:load-op)
-;;                                 (system asdf:system))
-;;   (when (string= (asdf:component-name system) "organ-mode")
-;;     (let ((pkg (find-package "ORGAN")))
-;;       (when pkg
-;;         ;; (setf (symbol-value (intern "*ORGAN-FILES*" pkg))
-;;         ;;       '((:path ("/home/mahmooz/brain/notes/")
-;;         ;;          :regex ".*\\.org"
-;;         ;;          :format "org-mode")))
-;;         (setf (symbol-value (intern "*ORGAN-FILES*" pkg))
-;;               `((:path (,(from-work "cltpt/tests/test2.org")
-;;                         ,(from-work "cltpt/tests/test.org"))
-;;                  :regex ".*\\.org"
-;;                  :format "org-mode")))
-;;         ))))
-
-;; (setf organ:*organ-files*
-;;       `((:path (,(from-work "cltpt/tests/test2.org")
-;;                 ,(from-work "cltpt/tests/test.org"))
-;;          :regex ".*\\.org"
-;;          :format "org-mode")))
 
 (lem:define-command books-find () ()
   (if organ:*organ-files*
