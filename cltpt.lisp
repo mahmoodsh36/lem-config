85ea379476cec9931d48293001ba5c85a717b649
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-08-21 21:52:19 +0300

    make organ recognize :defines

--- cltpt.lisp
+++ cltpt.lisp
@@ -8,3 +8,19 @@
    id))
 
 (setf cltpt/base:*id-to-attach-dir-func* 'my-id-to-attach-dir)
+
+;; make :defines work like :title for blocks
+(defmethod cltpt/base:text-object-init :around ((obj cltpt/org-mode:org-block) str1 match)
+  (call-next-method)
+  (let ((defines (cltpt/org-mode::org-block-keyword-value obj "defines")))
+    (when defines
+      (let ((node (cltpt/base:text-object-property obj :roam-node)))
+        (if node
+            (unless (cltpt/roam:node-title node)
+              (setf (cltpt/roam:node-title node) defines))
+            (setf (cltpt/base:text-object-property obj :roam-node)
+                  (cltpt/roam:make-node
+                   :id nil
+                   :title defines
+                   :desc nil
+                   :text-obj obj)))))))
