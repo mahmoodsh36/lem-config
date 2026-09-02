75e110f67a27b3e43a2e073d0ffeedd15a2b0755
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-09-02 10:20:26 +0300

    make :defines/:alias work like :title and #+alias work like #+title

--- cltpt.lisp
+++ cltpt.lisp
@@ -9,18 +9,45 @@
 
 (setf cltpt/base:*id-to-attach-dir-func* 'my-id-to-attach-dir)
 
-;; make :defines work like :title for blocks
+(defun titles-from-alist (alist)
+  (let ((res))
+    (dolist (pair alist)
+      (when (or (string-equal (car pair) "title")
+                (string-equal (car pair) "alias")
+                (string-equal (car pair) "defines"))
+        (push (cdr pair) res)))
+    (nreverse res)))
+
+;; make :defines/:alias work same as :title for blocks
 (defmethod cltpt/base:text-object-init :around ((obj cltpt/org-mode:org-block) str1 match)
   (call-next-method)
-  (let ((defines (cltpt/org-mode::org-block-keyword-value obj "defines")))
-    (when defines
+  (let* ((alist (cltpt/base:text-object-property obj :keywords-alist))
+         (terms (titles-from-alist alist)))
+    (when terms
       (let ((node (cltpt/base:text-object-property obj :roam-node)))
         (if node
-            (unless (cltpt/roam:node-title node)
-              (setf (cltpt/roam:node-title node) defines))
+            (setf (cltpt/roam:node-titles node) terms)
             (setf (cltpt/base:text-object-property obj :roam-node)
                   (cltpt/roam:make-node
                    :id nil
-                   :title defines
+                   :titles terms
                    :desc nil
                    :text-obj obj)))))))
+
+;; make #+alias work like #+title for documents
+(defmethod cltpt/base:text-object-finalize :around ((obj cltpt/org-mode::org-document))
+  (call-next-method)
+  (let* ((keywords (cltpt/base:text-object-property obj :keywords-alist))
+         (all-terms (titles-from-alist keywords))
+         (title (cltpt/base:text-object-property obj :title))
+         (node (cltpt/base:text-object-property obj :roam-node)))
+    (when all-terms
+      (if (null title)
+          (progn
+            (setf (cltpt/base:text-object-property obj :title) (car all-terms))
+            (if node
+                (setf (cltpt/roam:node-titles node) all-terms)
+                (setf (cltpt/base:text-object-property obj :roam-node)
+                      (cltpt/roam:make-node :id nil :titles all-terms :desc nil :text-obj obj))))
+          (when node
+            (setf (cltpt/roam:node-titles node) all-terms))))))
